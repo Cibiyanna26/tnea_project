fd9953650c3bc0d93d55c91caa062dce14f4b206
CibiyannaP <121782238+Cibiyanna26@users.noreply.github.com>
2023-05-10 02:24:51 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+const sub_but = document.getElementById("submit");
+
+// function search_code(){
+//     let college_code = document.getElementById("code").value;
+//     console.log(TNEA[college_code].name);
+//     document.getElementById("results1").innerHTML = TNEA[college_code].name;
+//     console.log(TNEA[college_code].rank);
+//     document.getElementById("results2").innerHTML = TNEA[college_code].rank;
+// }
+
+
+// sub_but.addEventListener('click',search_code);
